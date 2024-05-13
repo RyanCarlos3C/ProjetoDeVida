3268961f2e8d8b74c90c2a570b3781d287bb4897
Ryan Carlos Dos Santos <108525857+RyanCarlos3C@users.noreply.github.com>
2024-05-13 11:09:28 -0300

    s

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 500);
+    let segundos = Math.floor(tempoFinal / 50);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
